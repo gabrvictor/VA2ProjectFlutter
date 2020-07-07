1dc338c562533cde888bd62eb4b4ce2c952ac718
gabriel <gabriel19siqueira@gmail.com>
2020-07-07 18:31:51 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/ChatVA2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ChatVA2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/chat_screen.dart
@@ -0,0 +1,134 @@
+import 'dart:io';
+
+import 'package:ChatVA2/chat_message.dart';
+import 'package:ChatVA2/text_composer.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  FirebaseUser _currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+
+    FirebaseAuth.instance.onAuthStateChanged.listen((user) {
+      _currentUser = user;
+    });
+  }
+
+  Future<FirebaseUser> _getUser() async {
+    if(_currentUser != null) return _currentUser;
+
+    try {
+      final GoogleSignInAccount googleSignInAccount = 
+        await googleSignIn.signIn();
+      final GoogleSignInAuthentication googleSignInAuthentication = 
+        await googleSignInAccount.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+          idToken: googleSignInAuthentication.idToken, 
+          accessToken: googleSignInAuthentication.accessToken,
+      );
+
+      final AuthResult authResult = 
+        await FirebaseAuth.instance.signInWithCredential(credential);
+
+      final FirebaseUser user = authResult.user;
+      return user;
+    } catch (error) {
+      return null;
+    }
+  }
+
+  void _sendMessage({String text, File imgFile}) async {
+    final FirebaseUser user = await _getUser();
+
+    if(user == null){
+      _scaffoldKey.currentState.showSnackBar(
+        SnackBar(
+          content: Text('Não foi possível efetuar o login. Tente novamente!'),
+          backgroundColor: Colors.red,
+        )
+      );
+    }
+
+    Map<String, dynamic> data = {
+      'uid': user.uid,
+      'senderName': user.displayName,
+      'senderPhotoUrl': user.photoUrl,
+    };
+
+    if(imgFile != null){
+      StorageUploadTask task = FirebaseStorage.instance.ref().child(
+        DateTime.now().millisecondsSinceEpoch.toString()
+      ).putFile(imgFile);
+
+      // ignore: unused_local_variable
+      StorageTaskSnapshot taskSnapshot = await task.onComplete;
+      // ignore: unused_local_variable
+      String url = await taskSnapshot.ref.getDownloadURL();
+      data['imgUrl'] = url;
+    }
+
+    if(text != null) data['text'] = text;
+
+    Firestore.instance.collection('messages').add(data);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: Text('chatApp'),
+        elevation: 0,
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            // ignore: missing_required_param
+            child: StreamBuilder<QuerySnapshot>(
+              stream: Firestore.instance.collection('messages').snapshots(),
+              // ignore: missing_return
+              builder: (context, snapshot){
+                switch(snapshot.connectionState){
+                  case ConnectionState.none:
+                  case ConnectionState.waiting:
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  default:
+                    // ignore: unused_local_variable
+                    List<DocumentSnapshot> documents = 
+                        snapshot.data.documents.reversed.toList();
+
+                    return ListView.builder(
+                      itemCount: documents.length,
+                      reverse: true,
+                      itemBuilder: (context, index){
+                        return ChatMessage(documents[index].data);
+                      }
+                    );
+                }
+              },
+            ),
+          ),
+          TextComposer(_sendMessage),
+        ],
+      ),
+    );
+  }
+}
